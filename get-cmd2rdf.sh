4617b7000e36db92dafa409c51092e413650ab80
MKroese <1321738+MKroese@users.noreply.github.com>
2024-09-25 17:29:09 +0200

    Update get-cmd2rdf.sh to use knaw-huc

--- get-cmd2rdf.sh
+++ get-cmd2rdf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $CMD2RDF_SRC = "git" ]; then
-	git clone https://github.com/TheLanguageArchive/CMD2RDF.git
+	git clone https://github.com/knaw-huc/CMD2RDF.git
 else
 	$CMD2RDF_SRC
 fi
