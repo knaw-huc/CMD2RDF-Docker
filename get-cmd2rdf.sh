cd403f8140bfc6e0712db358d4a27fefc4141c63
Johan List <johan.list@di.huc.knaw.nl>
2025-07-30 10:04:22 +0200

    Point to CMD2RDF master

--- get-cmd2rdf.sh
+++ get-cmd2rdf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $CMD2RDF_SRC = "git" ]; then
-	git clone https://github.com/knaw-huc/CMD2RDF.git && cd CMD2RDF && git checkout generic-store-clients
+	git clone https://github.com/knaw-huc/CMD2RDF.git
 else
 	$CMD2RDF_SRC
 fi
