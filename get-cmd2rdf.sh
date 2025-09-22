cfaefe82cf1db7292541b4d136de6fdb5efc29ca
Johan List <johan.list@gmail.com>
2025-09-22 08:11:29 +0200

    Further simplified CMD2RDF container: no more running webapps (they're not working due to the GraphDB move and the API is the focus point for now)

--- get-cmd2rdf.sh
+++ get-cmd2rdf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $CMD2RDF_SRC = "git" ]; then
-	git clone https://github.com/knaw-huc/CMD2RDF.git && cd CMD2RDF && git checkout use-graphdb
+	git clone --branch use-graphdb https://github.com/knaw-huc/CMD2RDF.git
 else
 	$CMD2RDF_SRC
 fi
