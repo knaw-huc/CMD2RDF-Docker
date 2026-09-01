ca2abdc3f652cd86e1283dbb0abb594663015e0a
Johan List <johan.list@di.huc.knaw.nl>
2026-09-01 11:24:17 +0200

    Migrate to Java 25: download and install JDK 25 in the CMD2RDF Docker image; -XX:PermSize and -XX:MaxPermSize no longer exist for Java 25 - either drop them (chosen) or use -XX:MetaspaceSize or -XX:MaxMetaspaceSize

--- cmd2rdf-run.sh
+++ cmd2rdf-run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-java -Xms1g -Xmx2g -XX:PermSize=256m -XX:MaxPermSize=512m \
-  -jar /app/src/CMD2RDF/batch/target/Cmd2rdf.jar \
-  /app/src/CMD2RDF/batch/src/main/resources/cmd2rdf.xml
+java -Xms1g -Xmx2g \
+  -jar /app/src/CMD2RDF/batch/target/Cmd2rdf.jar /app/src/CMD2RDF/batch/src/main/resources/cmd2rdf.xml
